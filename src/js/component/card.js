626b3260a24ea965e11b393c04b46659a265b19e
Luisana Viamonte <luisanaviamonte@gmail.com>
2023-03-10 11:20:43 -0300

    se agrega icono en card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,15 +1,20 @@
 import React from "react";
-import imgCard from "../../img/ImgCard400x200.png"
+import imgCard from "../../img/ImgCard400x200.png";
+import PropTypes from "prop-types";
+import { AiOutlineHeart } from 'react-icons/ai';
 
-export const Card = () => {
+export const Card = (props) => {
     return (
         <div className="card">
             <img className="card-img-top" src={imgCard} alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">TEXT</p>
+                <a href="#" className="btn btn-primary"> <AiOutlineHeart /> </a>
             </div>
         </div>
     );
+};
+Card.propTypes = {
+    title: PropTypes.any
 };
